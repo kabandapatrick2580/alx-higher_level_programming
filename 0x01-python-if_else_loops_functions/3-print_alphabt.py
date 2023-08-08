bfe800cd8d4be86806d4bb141aadf0d12403a008
kabandapatrick2580 <sendsome2019@gmail.com>
2023-08-08 16:59:55 +0200

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-alphabet = ""
 for letter in range(ord('a'), ord('{'), +1):
     if chr(letter) in ['e', 'q']:
         continue
-    alphabet += chr(letter)
-print(f"{alphabet}", end="")
+    print("{}".format(chr(letter)), end="")
