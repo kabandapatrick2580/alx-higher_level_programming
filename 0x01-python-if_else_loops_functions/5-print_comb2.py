b36f1c09b5223b79ecfa852eb463776f057ebe4b
kabandapatrick2580 <sendsome2019@gmail.com>
2023-08-08 17:26:36 +0200

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-for number in range(0, 99):
-    print(f"{number:02d}, ", end="")
-    if number == 98:
-        print((number) + 1)
+for numb in range(0, 100):
+    if numb == 99:
+        print("{:d}".format(numb))
+    else:
+        print("{:02d}".format(numb), end=", ")
+
+    
+    
