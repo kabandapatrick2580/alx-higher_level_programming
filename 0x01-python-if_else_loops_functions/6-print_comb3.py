370b7a232d2d44b447a96611b5833afd011132b7
kabandapatrick2580 <sendsome2019@gmail.com>
2023-08-08 18:22:57 +0200

    5-print_comb2.py and 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for numb_one in range(10):
-    for numb_two in range(numb_one+1,10):
+    for numb_two in range(numb_one+1, 10):
         if numb_one == 8 and numb_two == 9:
             print(f"{numb_one:d}{numb_two:d}")
         else:
