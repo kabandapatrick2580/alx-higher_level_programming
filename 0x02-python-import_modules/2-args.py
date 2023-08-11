b688f55217989501bf9e2eff2f151cfe973bca4a
kabandapatrick2580 <sendsome2019@gmail.com>
2023-08-11 08:29:52 +0200

    2-args.py__

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    
+    import sys
+    
+    
+    argv_len = len(sys.argv) - 1
 
-for index, arg in enumerate(sys.argv[1:], start=1):
-    print(f"{index}: {arg}")
+    if argv_len == 0:
+        print("0 arguments.")
+    elif argv_len == 1:
+        print("1 argument:")
+    else:
+        print(f"{argv_len} arguments:")
+    for index, arg in enumerate(sys.argv[1:], start=1):
+        print(f"{index}: {arg}")
