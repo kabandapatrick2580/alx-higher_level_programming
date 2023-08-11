3283eaa7803824e301174d3a629ffce0c32080d5
kabandapatrick2580 <sendsome2019@gmail.com>
2023-08-11 08:48:06 +0200

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
 
-def arguments_sum(args):
-    result = sum(map(int, args))
-    return result
+    def arguments_sum(args):
+        result = sum(map(int, args))
+        return result
 
 
-arguments = sys.argv[1:]
-summation = arguments_sum(arguments)
-print(summation)
+    arguments = sys.argv[1:]
+    summation = arguments_sum(arguments)
+    print(summation)
