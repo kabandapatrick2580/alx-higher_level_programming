71531617abb8158ab540576e8e5b2d9d27bd21d9
kabandapatrick2580 <sendsome2019@gmail.com>
2023-08-12 09:28:57 +0200

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
 
-    for n in my_list:
+    for i, n in enumerate(my_list):
         print("{}".format(n))
 
 
-my_list = [1, 2, 3, 4, 5]
-print_list_integer(my_list)
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    print_list_integer(my_list)
