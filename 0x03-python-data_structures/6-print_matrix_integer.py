2d3f2514d84e3e4763aac1513e4710d01544d2a5
kabandapatrick2580 <sendsome2019@gmail.com>
2023-08-14 19:25:05 +0200

    7-add_tuple.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col_num in row:
             if col_num == row[-1]:
-                print("{:d}".format(col_num), end ="")
+                print("{:d}".format(col_num), end="")
             else:
-                print("{:d}".format(col_num), end = " ")
+                print("{:d}".format(col_num), end=" ")
         print()
