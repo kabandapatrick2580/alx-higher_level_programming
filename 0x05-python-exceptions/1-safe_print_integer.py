340aba088ae9af8a0e486697c80f9886ac49bfa3
kabandapatrick2580 <sendsome2019@gmail.com>
2023-08-30 16:43:34 +0200

    some changes were made

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print(f"{value:d}")
+        print(f"{int(value):d}")
         return True
     except (TypeError, ValueError):
         return False
