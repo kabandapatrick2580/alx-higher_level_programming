66e12f9fe13cc5e91b7430d0bddfdfa97355feb6
kabandapatrick2580 <sendsome2019@gmail.com>
2023-11-13 23:43:36 +0200

    semistandard errors fixed

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-let li_one = "C is fun";
-let li_two = "Python is cool";
-let li_three = "JavaScript is amazing";
+const lione = 'C is fun';
+const litwo = 'Python is cool';
+const lithree = 'JavaScript is amazing';
 
-console.log(li_one);
-console.log(li_two);
-console.log(li_three);
+console.log(lione);
+console.log(litwo);
+console.log(lithree);
--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,13 @@
-!#/usr/bin/node
-function printMessage(){
-	const argumentsNumber = arguments.length;
+#!/usr/bin/node
+function printMessage () {
+  const argumentsNumber = arguments.length;
 
-	if( argumentsNumber === 0) {
-		console.log("No argument");
-	}else if(argumentsNumber === 1){
-		console.log("Argument found");
-	}else{
-		console.log("Arguments found");
-	}
+  if (argumentsNumber === 0) {
+    console.log('No argument');
+  } else if (argumentsNumber === 1) {
+    console.log('Argument found');
+  } else {
+    console.log('Arguments found');
+  }
 }
-printMessage('Lucy')
+printMessage('Lucy', 'James');
