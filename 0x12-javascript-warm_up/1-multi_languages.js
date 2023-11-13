3337e30b43338df81c86509fcdead7e8edca121c
kabandapatrick2580 <sendsome2019@gmail.com>
2023-11-13 18:25:02 +0200

    1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+let li_one = "C is fun";
+let li_two = "Python is cool";
+let li_three = "JavaScript is amazing";
+
+console.log(li_one);
+console.log(li_two);
+console.log(li_three);
