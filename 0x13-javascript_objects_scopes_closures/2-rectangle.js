b19d9377aa63a2d9c19a51652a797f6882ebba74
kabandapatrick2580 <sendsome2019@gmail.com>
2023-11-14 21:05:39 +0200

    If not running I am proceeding woth other tasks

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (typeof w !== 'number' || typeof h !== 'number' || w <= 0 || h <= 0) {
-      return {};
-    } else {
-    this.width = w;
-    this.height = h;
+    if (typeof w === 'number' && typeof h === 'number' && h > 0 && w > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
 }
