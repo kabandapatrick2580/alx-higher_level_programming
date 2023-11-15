476c6d61aa1699b2c0419640467bef52a7b6b4d4
kabandapatrick2580 <sendsome2019@gmail.com>
2023-11-15 18:30:32 +0200

    5-to_integer.js fixed for good

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 function to_integer () {
   const argum = process.argv[2];
-  const convertedNum = Number(argum);
-  if (!isNaN(convertedNum)) {
-    console.log(`My number : ${convertedNum}`);
-  } else {
+  const convertedNum = parseInt(argum);
+  if (isNaN(convertedNum)) {
     console.log('Not a number');
+  } else {
+    console.log(`My number : ${convertedNum}`);
   }
 }
 to_integer();
