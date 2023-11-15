a0aa800b9b92c83ce5e043fb5dd43417f9c598bf
kabandapatrick2580 <sendsome2019@gmail.com>
2023-11-15 18:26:46 +0200

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
-const argum = process.argv[2];
-const convertedNum = Number(argum);
-if (!isNaN(convertedNum)) {
-  console.log(`My number : ${convertedNum}`);
-} else {
-  console.log('Not a number');
+function to_integer () {
+  const argum = process.argv[2];
+  const convertedNum = Number(argum);
+  if (!isNaN(convertedNum)) {
+    console.log(`My number : ${convertedNum}`);
+  } else {
+    console.log('Not a number');
+  }
 }
+to_integer();
