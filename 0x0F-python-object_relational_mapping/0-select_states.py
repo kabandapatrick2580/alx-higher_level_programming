1f86466d99446c422c665d4b03658cf9fd0940b2
kabandapatrick2580 <sendsome2019@gmail.com>
2023-11-16 11:36:38 +0200

    0-select_states.py updated

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,18 @@
+#!/usr/bin/python3
 import MySQLdb
-db = MySQLdb.connect(host="localhost", user="patrick", passwd="Bijdad@1234", db="hbtn_0e_0_usa", port=3306)
+import sys
+# Check if all required command-line arguments are provided
+if len(sys.argv) != 4:
+    sys.exit(1)
+
+# Get command-line arguments
+username, password, database = sys.argv[1:4]
+
+db = MySQLdb.connect(host="localhost", user="root", passwd="Bijdad@1234", db="hbtn_0e_0_usa", port=3306)
 
 cursor = db.cursor()
 
-cursor.execute("SELECT * FROM states")
+cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
 results = cursor.fetchall()
 
