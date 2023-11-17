d17ee3277a678a39560954795da125879ec11de5
kabandapatrick2580 <sendsome2019@gmail.com>
2023-11-17 11:59:47 +0200

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,22 +1,65 @@
 #!/usr/bin/python3
+"""
+This script connects to a MySQL database and retrieves a list of states
+from the 'states' table based on a partial match of the state name.
+The script expects four command-line arguments: MySQL username, MySQL password,
+MySQL database name, and the partial name of the state.
+
+Usage:
+    ./script.py <username> <password> <database> <state>
+
+Arguments:
+    username: MySQL username
+    password: MySQL password
+    database: MySQL database name
+    state: Partial name of the state for filtering
+
+Example:
+    ./script.py root secret mydatabase New
+"""
+
 import MySQLdb
 import sys
-# Check if all required command-line arguments are provided
-if len(sys.argv) != 5:
-    sys.exit(1)
 
-# Get command-line arguments
-username, password, database, state = sys.argv[1:5]
+if __name__ == '__main__':
+    # Check if all required command-line arguments are provided
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <pwd> <Db> <state>".format(sys.argv[0]))
+        sys.exit(1)
 
-db = MySQLdb.connect(host="localhost", user="root", passwd="Bijdad@1234", db="hbtn_0e_0_usa", port=3306)
+    # Get command-line arguments
+    username, pwd, Db, state = sys.argv[1:5]
 
-cursor = db.cursor()
-command = ("SELECT * FROM states WHERE name  LIKE %s")
+    try:
+        # Connect to the MySQL server
+        db = MySQLdb.connect(
+                host="localhost",
+                user=username,
+                passwd=pwd,
+                db=Db,
+                port=3306
+                )
+        cursor = db.cursor()
 
-cursor.execute(command, (state,))
+        # SQL command with a parameterized query to filter states by name
+        command = "SELECT * FROM states WHERE name LIKE %s"
 
-results = cursor.fetchall()
+        # Execute the SQL query with the parameter
+        cursor.execute(command, (state,))
 
-for row in results:
-    print(row)
-db.close()
+        # Fetch all the results
+        results = cursor.fetchall()
+
+        # Display the results
+        for row in results:
+            print(row)
+
+    except MySQLdb.Error as err:
+        # Handle any MySQL database errors and print an error message
+        print("MySQL Error:", err)
+
+    finally:
+        # Ensure that the database connection is closed, whether or not an
+        # error occurred
+        if 'db' in locals():
+            db.close()
