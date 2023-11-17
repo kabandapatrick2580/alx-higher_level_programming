83fb8149ac2a04877567303fd309922896a1a5d5
kabandapatrick2580 <sendsome2019@gmail.com>
2023-11-17 11:22:44 +0200

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,22 +1,61 @@
 #!/usr/bin/python3
+"""
+This script connects to a MySQL database and retrieves a list of states
+from the 'states' table,  whose name
+starts with 'N'. The script assumes three command-line arguments:
+MySQL username, MySQL password, and MySQL database name.
+
+Usage:
+    ./script.py <username> <password> <database>
+
+Arguments:
+    username: MySQL username
+    password: MySQL password
+    database: MySQL database name
+
+Example:
+    ./script.py root secret mydatabase
+"""
+
 import MySQLdb
 import sys
-# Check if all required command-line arguments are provided
-if len(sys.argv) != 4:
-    sys.exit(1)
 
-# Get command-line arguments
-username, password, database = sys.argv[1:4]
+if __name__ == '__main__':
+    # Check if all required command-line arguments are provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
 
-db = MySQLdb.connect(host="localhost", user="root", passwd="Bijdad@1234", db="hbtn_0e_0_usa", port=3306)
+    # Get command-line arguments
+    username, password, database = sys.argv[1:4]
 
-cursor = db.cursor()
+    try:
+        # Connect to the MySQL server
+        db = MySQLdb.connect(
+            host="localhost",
+            user=username,
+            passwd=password,
+            db=database,
+            port=3306
+        )
+        cursor = db.cursor()
 
-cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+        # Execute the SQL query to select states starting with 'N'
+        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'")
 
-results = cursor.fetchall()
+        # Fetch all the results
+        results = cursor.fetchall()
 
-for row in results:
-    print(row)
+        # Display the results
+        for row in results:
+            print(row)
 
-db.close()
+    except MySQLdb.Error as err:
+        # Handle any MySQL database errors and print an error message
+        print("MySQL Error:", err)
+
+    finally:
+        # Ensure that the database connection is closed, whether or not an
+        # error occurred
+        if 'db' in locals():
+            db.close()
