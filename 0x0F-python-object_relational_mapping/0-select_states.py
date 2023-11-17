ffbc5550ecbff4ab60273f7fa43f43756e6e6ef3
kabandapatrick2580 <sendsome2019@gmail.com>
2023-11-17 10:12:03 +0200

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,17 +10,27 @@ if __name__ == '__main__':
     # Get command-line arguments
     username, password, database = argv[1:4]
 
-    db = MySQLdb.connect(
-            host="localhost", user=argv[1], passwd=argv[2],
-            db=argv[3], port=3306)
+    try: 
+        db = MySQLdb.connect(
+                host="localhost", user=argv[1], passwd=argv[2],
+                db=argv[3], port=3306)
 
-    cursor = db.cursor()
+        cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    results = cursor.fetchall()
+        results = cursor.fetchall()
 
-    for row in results:
-        print(row)
+        for row in results:
+            print(row)
 
-    db.close()
+    
+    except MySQLdb.Error as err:
+        # Handle any MySQL database errors and print an error message
+        print("Error:", err)
+
+    finally:
+        # Ensure that the database connection is closed, whether or not an
+        # error occurred
+        if 'db' in locals():
+            db.close()
