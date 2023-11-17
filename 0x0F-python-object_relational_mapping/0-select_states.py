2b907b6987c61f9240bcb36ac592c074638b512b
kabandapatrick2580 <sendsome2019@gmail.com>
2023-11-17 10:01:56 +0200

    pycodestyle corrected

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,11 +2,17 @@
 import MySQLdb
 from sys import argv
 
-if __name__ =='__main__':
-   
+if __name__ == '__main__':
+    # Check if all required command-line arguments are provided
+    if len(argv) != 4:
+        exit(1)
+
+    # Get command-line arguments
+    username, password, database = argv[1:4]
+
     db = MySQLdb.connect(
-        host="localhost", user= argv[1], passwd=argv[2], 
-        db=argv[3], port=3306)
+            host="localhost", user=argv[1], passwd=argv[2],
+            db=argv[3], port=3306)
 
     cursor = db.cursor()
 
