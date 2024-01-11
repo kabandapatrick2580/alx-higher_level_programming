d533a6740122498cee12941b64c2cb5daf9e10d9
kabandapatrick2580 <sendsome2019@gmail.com>
2024-01-11 11:50:53 +0200

    0-script.js

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,5 @@
+// Select the <header> element using the querySelector method
+const headerElement = document.querySelector('header');
+
+// Set the text color of the selected header element to red (#FF0000)
+headerElement.style.color = '#FF0000';
